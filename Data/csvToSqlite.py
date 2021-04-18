a6946e491293e48b686aa47b610dd956056b81e4
JoxeArruebarrenaAizpurua <arru.aizpu@gmail.com>
2021-04-18 22:38:04 +0100

    csv to sqlite + requirements

---
+++ Data/csvToSqlite.py
@@ -0,0 +1,17 @@
+import csv_to_sqlite, csv
+
+with open('companiesList.csv', newline='') as file:
+    reader = csv.reader(file)
+    companiesList = list(reader)
+
+options = csv_to_sqlite.CsvOptions(typing_style='full',encoding='windows-1250')
+inputFiles = []
+for i in companiesList:
+    if i[1] != 'Ticker':
+        companyCsv = 'Companies_CSV/' + i[1] + '.csv'
+        inputFiles = [companyCsv]
+        #inputFiles.append(companyCsv)
+
+        csv_to_sqlite.write_csv(inputFiles, 'companies_historical_info.sqlite', options)
+
+print()
